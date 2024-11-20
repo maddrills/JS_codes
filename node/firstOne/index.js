65e2fecfd6fd59f65a23ed54da28bb155f6fe6f4
maddrills <mathewfvv@gmail.com>
2024-11-20 14:41:03 +0530

    Post and / and lst resort post route

--- node/firstOne/index.js
+++ node/firstOne/index.js
@@ -15,13 +15,28 @@ const app = express();
 //   res.send("Woooow we got a response");
 // });
 
+//home route
+app.get("/", (req, res) => {
+  res.send("You are at the home page");
+});
+
 // get request
 app.get("/cats", (req, res) => {
   res.send("Shaten butain");
+});
+
+//post
+app.post("/pstCat", (req, res) => {
+  res.send("meeeuuu post");
 });
 
 //now create a port to listen for incoming requests
 
 app.listen(8080, () => {
   console.log("Listing on port 8080!");
+});
+
+//genaric response has to be the last requeat last resort
+app.get("*", (req, res) => {
+  res.send("I dont know this path");
 });
