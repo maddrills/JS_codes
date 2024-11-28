0bf92de240fb0628219df6da5ff508b43ae964ca
maddrills <mathewfvv@gmail.com>
2024-11-28 17:40:31 +0530

    Schema Constrraints

--- MongoDB/one/products.js
+++ MongoDB/one/products.js
@@ -7,3 +7,100 @@ mongoose
   .catch((error) => {
     console.log("connection failed", error);
   });
+
+//enforce for sanity
+// const productSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   price: {
+//     type: Number,
+//   },
+// });
+
+// default value
+// const productSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   price: {
+//     type: Number,
+//   },
+//   onSale: {
+//     type: Boolean,
+//     default: false,
+//   },
+// });
+
+// max length
+// const productSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//     maxLength: 20,
+//   },
+//   price: {
+//     type: Number,
+//   },
+//   onSale: {
+//     type: Boolean,
+//     default: false,
+//   },
+// });
+
+// have array in mongo
+// const productSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//     maxLength: 20,
+//   },
+//   price: {
+//     type: Number,
+//   },
+//   onSale: {
+//     type: Boolean,
+//     default: false,
+//   },
+//   //can save a array with categories : ['Cycling','Safety',...]
+//   //categories: [String],
+//   //could also
+//   categories: {
+//     type: [String],
+//     default: ["cycling"],
+//   },
+// });
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxLength: 20,
+  },
+  price: {
+    type: Number,
+  },
+  onSale: {
+    type: Boolean,
+    default: false,
+  },
+  //can save a array with categories : ['Cycling','Safety',...]
+  //categories: [String],
+  //could also
+  categories: {
+    type: [String],
+    default: ["cycling"],
+  },
+  qty: {
+    online: {
+      type: Number,
+      default: 0,
+    },
+    inStore: {
+      type: Number,
+      default: 0,
+    },
+  },
+});
